9140bd5a3c1d31342ae7cd0d2317978e3a984945
icarosadero <38845151+icarosadero@users.noreply.github.com>
2019-05-25 18:59:13 -0300

    Update and rename schwarzchild3.py to schwarzchild.py

--- schwarzchild.py
+++ schwarzchild.py
@@ -5,30 +5,35 @@ import plotly.graph_objs as go
 from scipy.integrate import odeint
 
 """
-Paramemters
-m2, r0 = -2.0, 1.0
-r1,r2 = -1.0, 1.0
-M = 2*np.sqrt(-r0*m2*(r1+r2)/2)
+Values for the constants of motion.
+The signal of m2 is tinkered in a way
+to output a timelike trajectory
 """
-r0,M,m2 = 1.0, 5.0, 1.0
+r0,M,m2 = 5.0, 30.0, 1.0
 a,b,c = -r0*m2/2.0, (M*M)/4.0, -(3.0/8)*M*M*r0
-#Try circular orbit
+#Preset for a circular orbit
 #a,b,c = -1, 0, 100 works with M=5**4
 
-#t = np.linspace(0,100000,10000)
 t = np.linspace(0,1e+5,1000)
 y0 = np.array([243,0.0,0.0])
 
-#def poly(r): return (r0*m2/2)*(r-r1)*(r-r2)
+"""
+Polynomial in the numerator of the
+equation for the second derivative of the radius
+"""
 def poly(r): return a*r**2.0 + b*r + c
 roots = np.roots([a,b,c])
 print('Roots: ',roots)
 
+"""
+Differential equations for radius and
+time respectively
+"""
 def R(r):
     return poly(r)/r**4
 
 def P(r):
-    return M/(2*r*r) #for some reason works for M->M**4
+    return M/(2*r*r)
 
 def f(y,t):
     """
@@ -37,7 +42,7 @@ def f(y,t):
     r,u,phi = y
     return np.array([u,R(r),P(r)])
 
-sol = odeint(f, y0, t)
+sol = odeint(f, y0, t) #Solves the differetial equation
 
 np.savetxt('orbitValues3.txt', sol)
 
@@ -51,7 +56,7 @@ data = [
         mode = 'lines',
         line=dict(
             color='purple'
-        ) 
+        )
 ),
     go.Scatterpolar(
         r = sol[:,0],
@@ -79,4 +84,4 @@ layout = go.Layout(
 )
 
 py.plot(go.Figure(data = [data[0]]), filename = 'quadratic4.html')
-py.plot(go.Figure(data = [data[1]], layout=layout), filename = 'orbit4.html')
+py.plot(go.Figure(data = [data[1]], layout=layout), filename = 'orbit2.html')
